df87a04f41bbd0e8c439b11aec81784f4329d174
Diego5RG-dev <dgilis@danielcastelao.org>
2025-09-24 11:37:41 +0200

    Funcion pago y selecccion hecha

--- src/CoffeeMachineState.kt
+++ src/CoffeeMachineState.kt
@@ -2,11 +2,10 @@ sealed class CoffeeMachineState {
     object Idle : CoffeeMachineState() /* como es de espera le coloco object*/
     data class CoffeeSelection(val coffeeType: String, val sugarLevel: Int) : CoffeeMachineState()
     object MakingCoffee : CoffeeMachineState()
-    data class ServingCoffee(val coffeeType: String) : CoffeeMachineState()
+    object ServingCoffee: CoffeeMachineState()
     data class CoffeeFinal (val cambio: Double) : CoffeeMachineState()
 
     /*En este caso he optado por poner "data class a serving coffee, final y seleccion porque dependiendo del caso estos
-    van a reciibir parametros, final, el cambio que va a recibir, selection, la seleccion del cafe, y serving un tipo de preparacion,
-    las dos otras no deberian recibir datos
+    van a reciibir parametros, final, el cambio que va a recibir, selection, la seleccion del cafe, y el nivel de azucar
      */
 }
