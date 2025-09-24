bcb732b2a94a6c6e4068131095428fd21942c434
Diego5RG-dev <dgilis@danielcastelao.org>
2025-09-24 11:45:18 +0200

    Funcion pago y selecccion hecha actualizar metodos de pago insuficiente

--- src/CoffeeMachine.kt
+++ src/CoffeeMachine.kt
@@ -1,5 +1,6 @@
 object CoffeeMachine {
     private var CurrentState: CoffeeMachineState = CoffeeMachineState.Idle
+    var precioCafeSeleccionado: Double = 0.0
 
     fun MakeCoffee() {
         println("Estado actual: $CurrentState")
@@ -21,7 +22,6 @@ object CoffeeMachine {
                 CurrentState = CoffeeMachineState.CoffeeFinal(0.50)
                 println("Su cafe esta listo, aqui tiene su cambio")
             }
-
             is CoffeeMachineState.CoffeeSelection -> {}
             is CoffeeMachineState.MakingCoffee -> {}
             is CoffeeMachineState.ServingCoffee -> {}
@@ -38,30 +38,31 @@ object CoffeeMachine {
             "Macchiato" to 1.20,
             "Mocha" to 1.30
         )
-        println("Escoge el cafe;")
+        println("Escoge el cafe (escribe el nombre):")
         for ((nombre, precio) in listaDeCafes) {
-            println("- $nombre: \$$precio")
-            print("Introduce el nombre de tu café: ")
+            println("- $nombre: €$precio")
         }
-
+        print("Introduce el nombre de tu café: ")
         val opcion = readLine()?.trim()
         val cafeSeleccionado = listaDeCafes.find { it.first.equals(opcion, ignoreCase = true) }
         if (cafeSeleccionado != null) {
             val (nombre, precio) = cafeSeleccionado
+            precioCafeSeleccionado = precio
             println("Has elegido: $nombre - €$precio")
         } else {
             println("Opción no válida.")
+            precioCafeSeleccionado = 0.0
         }
     }
 
     fun pago() {
         println("Por favor, inserte el dinero")
         val dineroInsertado = readLine()?.toDoubleOrNull() ?: 0.0
-        if (dineroInsertado >= 1.20) {
-            val cambio = dineroInsertado - 1.20
+        if (dineroInsertado >= precioCafeSeleccionado) {
+            val cambio = dineroInsertado - precioCafeSeleccionado
             println("Pago aceptado. Su cambio es: €$cambio")
         } else {
-            println("Dinero insuficiente. Por favor, inserte al menos €1.20")
+            println("Dinero insuficiente. Por favor, inserte al menos €$precioCafeSeleccionado")
         }
     }
 }
